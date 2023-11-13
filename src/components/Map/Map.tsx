0092b2651e4a3ea681ecda0cef6f73bee907e7a1
sankomil <43489998+sankomil@users.noreply.github.com>
2023-11-13 17:41:07 +0800

    feat(map): use different library

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,33 +1,24 @@
 import React from "react";
-import GoogleMapReact from "google-map-react";
-import { MapPin } from "../MapPin";
+import { Map as GoogleMap, Marker } from "@vis.gl/react-google-maps";
 
 export const Map: React.FC = () => {
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
-  };
-
   return (
     <div
       style={{
         height: "100vh",
-        width: "50vw",
-        left: "20vw",
-        top: 0,
+        width: "100vw",
+        position: "relative",
         border: "1px solid black",
       }}
     >
-      <GoogleMapReact
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_ID || "" }}
+      <GoogleMap
+        zoom={6}
+        center={{ lat: 53.54992, lng: 10.00678 }}
+        gestureHandling={"greedy"}
+        disableDefaultUI={true}
       >
-        <MapPin label="Test" lat={10.99835602} lng={77.01502627} />
-      </GoogleMapReact>
+        <Marker position={{ lat: 53.54992, lng: 10.00678 }} />
+      </GoogleMap>
     </div>
   );
 };
