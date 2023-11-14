25c03be0e9b33200bfd0e0a2d138c4f9f438f74f
sankomil <43489998+sankomil@users.noreply.github.com>
2023-11-14 14:29:05 +0800

    feat(sidebar): use hooks to fetch route data

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,44 +1,81 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import "./index.css";
+import { useGetRoute } from "../../hooks";
 
 export const Sidebar: React.FC = () => {
+  const [origin, setOrigin] = useState<string>("");
+  const [destination, setDestination] = useState<string>("");
+  const { getPathToken } = useGetRoute();
+
+  const onSubmit = async (e?: FormEvent<HTMLFormElement>): Promise<void> => {
+    if (e) {
+      e?.preventDefault();
+    }
+    await getPathToken({ origin, destination });
+  };
+
+  const onCancel = (): void => {
+    setOrigin("");
+    setDestination("");
+  };
+
   return (
-    <div className="sidebar-container p-3">
-      <h1 className="display-5 text-center">Location points</h1>
-      <div className="d-flex flex-column mt-5">
-        <label htmlFor="start-point" className="mb-1">
-          Start point
-        </label>
-        <div className="d-flex align-items-center">
-          <input id="start-point" className="w-100" />
+    <form onSubmit={onSubmit}>
+      <div className="sidebar-container p-3">
+        <h1 className="display-5 text-center">Location points</h1>
+        <div className="d-flex flex-column mt-5">
+          <label htmlFor="start-point" className="mb-1">
+            Start point
+          </label>
+          <div className="d-flex align-items-center">
+            <input
+              id="start-point"
+              className="w-100"
+              onChange={(e) => setOrigin(e.target.value)}
+            />
+            <button
+              type="button"
+              className="close btn btn btn-outline-secondary ms-3 btn-sm"
+              aria-label="Close"
+              onClick={() => setOrigin("")}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        </div>
+        <div className="d-flex flex-column mt-5">
+          <label htmlFor="end-point" className="mb-1">
+            End point
+          </label>
+          <div className="d-flex align-items-center">
+            <input
+              id="end-point"
+              className="w-100"
+              onChange={(e) => setDestination(e.target.value)}
+            />
+            <button
+              type="button"
+              className="close btn btn btn-outline-secondary ms-3 btn-sm"
+              aria-label="Close"
+              onClick={() => setDestination("")}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        </div>
+        <div className="d-flex flex-row mt-4">
           <button
+            className="btn btn-success me-3"
             type="button"
-            className="close btn btn btn-outline-secondary ms-3 btn-sm"
-            aria-label="Close"
+            onClick={() => onSubmit()}
           >
-            <span aria-hidden="true">&times;</span>
+            Submit
+          </button>
+          <button className="btn btn-danger" onClick={() => onCancel()}>
+            Cancel
           </button>
         </div>
       </div>
-      <div className="d-flex flex-column mt-5">
-        <label htmlFor="end-point" className="mb-1">
-          End point
-        </label>
-        <div className="d-flex align-items-center">
-          <input id="end-point" className="w-100" />
-          <button
-            type="button"
-            className="close btn btn btn-outline-secondary ms-3 btn-sm"
-            aria-label="Close"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-      </div>
-      <div className="d-flex flex-row mt-4">
-        <button className="btn btn-success me-3">Submit</button>
-        <button className="btn btn-danger">Cancel</button>
-      </div>
-    </div>
+    </form>
   );
 };
